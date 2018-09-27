91fab7a9e8e2fd49f13555c47eb29fc094354f61
Joe Blank <joe.blank@live.com>
2018-09-27 15:05:30 -0600

    nav bar polish and routing

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -20,68 +20,89 @@ export default class Nav extends Component {
     })
   }
 
+  changeRoute = (path) => {
+    this.setState({ expanded: !this.state.expanded });
+    console.log(this.props)
+    window.location.hash = `#/${path}`
+  }
+
   render() {
     const { expanded } = this.state;
     return (
       <div className='nav-container'>
         <div className="parent2">
-          <div className="test1" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(125px,0px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('build')}
+            className="test1"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(125px,0px)'
+            } : null
+            }>
             <i className="fas fa-cogs fa-2x"></i>
           </div>
-          <div className="test2" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(105px,-60px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('text-over-images')}
+            className="test2"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(105px,-60px)'
+            } : null
+            }>
             <i className="far fa-object-ungroup fa-2x"></i>
           </div>
-          <div className="test3" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(60px,-105px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('images')}
+            className="test3"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(60px,-105px)'
+            } : null
+            }>
             <i className="fas fa-images fa-2x"></i>
           </div>
-          <div className="test4" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(0px,-125px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('')}
+            className="test4"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(0px,-125px)'
+            } : null
+            }>
             <i className="fas fa-home fa-2x"></i>
           </div>
-          <div className="test5" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(0px,-70px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('spacing-layout')}
+            className="test5"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(0px,-70px)'
+            } : null
+            }>
             <i className="fas fa-layer-group" style={{ fontSize: '24px' }}></i>
           </div>
-          <div className="test6" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(46px,-46px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('color-theme')}
+            className="test6"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(46px,-46px)'
+            } : null
+            }>
             <i className="fas fa-palette" style={{ fontSize: '24px' }}></i>
           </div>
-          <div className="test7" style={expanded ? {
-            'backgroundColor': 'gray',
-            'transform': 'translate(70px,0px)'
-          } : null
-          }>
+          <div
+            onClick={() => this.changeRoute('fonts')}
+            className="test7"
+            style={expanded ? {
+              'backgroundColor': 'gray',
+              'transform': 'translate(70px,0px)'
+            } : null
+            }>
             <i className="fas fa-font" style={{ fontSize: '24px' }}></i>
           </div>
           <div className="mask2" onClick={this.expand}>
-            {
-              this.state.expanded ? (
-                <i className="fas fa-compress fa-3x"></i>
-              ) : (
-                  <i className="fas fa-expand-arrows-alt fa-3x"></i>
-                )
-            }
+            <i className="fas fa-ellipsis-v fa-3x"></i>
           </div>
         </div>
       </div>
